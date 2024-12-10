2608863cf69d6e49d05ea5b60e7c2528b466d2e3
tamphuc06012004@gmail.com <tamphuc06012004@gmail.com>
2024-12-10 09:00:50 +0700

    Them tac gia vao file HelloGit

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
 	public static void main (String [] args) {
 	System.out.println("Welcome to Version Control System with Git");
+	System.out.println(" Author : Linus Torvald");
 	}
 }
